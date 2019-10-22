86f051fdfadd8def0b9fd8ce7f920d3a298ac7dd
mozhnyy <48670931+mozhnyy@users.noreply.github.com>
2019-10-22 16:12:08 +0400

    all

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+burger.addEventListener('click',function () {
+    menu.classList.toggle('show');
+});
